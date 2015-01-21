f0685c0c464595e95db2f261f5eab997842e8167
Bob Yexley <bob@yexley.net>
2015-01-21 10:10:24 -0500

    Configuration solution...now I can load up a configured database connection string

---
+++ src/api/config/index.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var _ = require("lodash");
+var fs = require("fs");
+var path = require("path");
+
+var env = process.env.NODE_ENV || "dev";
+
+var defaultConfig = {};
+var envConfig = {};
+
+var defaultConfigPath = path.join(__dirname, "config.json");
+var envConfigPath = path.join(__dirname, env + ".config.json");
+
+if (fs.existsSync(defaultConfigPath)) {
+    defaultConfig = require(defaultConfigPath);
+}
+
+if (fs.existsSync(envConfigPath)) {
+    envConfig = require(envConfigPath);
+}
+
+var configData = _.extend({}, defaultConfig, envConfig);
+
+var config = require("configya")(configData);
+
+_.extend(config, {
+    connectionString: function () {
+        var connectionString = "",
+            connectionStringTemplate = "postgres://{user}@{host}/{database}";
+
+        // set lodash template interpolation
+        _.templateSettings.interpolate = /{([\s\S]+?)}/g;
+
+        if (this.postgres) {
+            connectionString = _.template(connectionStringTemplate, this.postgres);
+        }
+
+        return connectionString;
+    }
+});
+
+module.exports = config;
